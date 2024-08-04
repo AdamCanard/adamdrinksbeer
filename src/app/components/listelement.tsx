b4c4edfb97f57957a378bd2472eb5d1b6ed20e15
AdamCanard <azcunard@gmail.com>
2024-08-03 22:16:57 -0600

    jeremy changes

--- src/app/components/listelement.tsx
+++ src/app/components/listelement.tsx
@@ -7,25 +7,26 @@ export default function ListElement(props: { data: any; type: string }) {
   };
   return (
     <>
-      {props.type == "Drank" ? (
-        <div
-          className="flex w-full h-12 border-2 justify-between items-center p-2 hover:cursor-pointer"
-          onClick={handleClick}
-        >
-          <div>{props.data.Beer}</div>
-          <div>{props.data.Brewery}</div>
-          <div>{props.data.Rating}</div>
-        </div>
-      ) : (
-        <div
-          className="flex w-full h-12 border-2 justify-between items-center p-2"
-          onClick={handleClick}
-        >
-          <div>{props.data.Beer}</div>
-          <div>{props.data.Brewery}</div>
-          <div>{props.data.By}</div>
-        </div>
-      )}
+      <div
+        className="flex w-full h-12 border-2 justify-between items-center p-2 hover:cursor-pointer"
+        onClick={handleClick}
+      >
+        {props.type == "Drank" && (
+          <>
+            <div>{props.data.Beer}</div>
+            <div>{props.data.Brewery}</div>
+            <div>{props.data.By}</div>
+          </>
+        )}
+
+        {props.type == "Drink" && (
+          <>
+            <div>{props.data.Beer}</div>
+            <div>{props.data.Brewery}</div>
+            <div>{props.data.Rating}</div>
+          </>
+        )}
+      </div>
     </>
   );
 }
