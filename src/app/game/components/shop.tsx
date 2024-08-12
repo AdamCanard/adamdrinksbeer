30c8108181a61ab0edf27eef044a5f4f061216d2
AdamCanard <azcunard@gmail.com>
2024-08-12 13:18:44 -0600

    added unlocks

--- src/app/game/components/shop.tsx
+++ src/app/game/components/shop.tsx
@@ -12,6 +12,13 @@ export default function Shop() {
 interface Upgrade {
   Title: string;
   Amount: number;
+  initCost: number;
+  SPS: number;
+}
+interface Unlock {
+  Title: string;
+  Desc: string;
+  Trigger: boolean;
 }
 
 //move to DB eventually
@@ -21,24 +28,58 @@ const UpgradeList: Upgrade[] = [
   //That segment or your soul will take small sips of your beer for you
 
   // Unlocks:
+  // Fragmented sips
   // Quantam Frat (compounding)
-  { Title: "Quantum Sip", Amount: 0 },
+  { Title: "Quantum Sip", Amount: 0, initCost: 10, SPS: 0.1 },
   //Every Friend you buy invites a friend over to your house who will help you drink beers
 
   // Unlocks:
   // Better Friends (static)
-  { Title: "Friend", Amount: 0 },
+  { Title: "Friend", Amount: 0, initCost: 100, SPS: 1 },
 
   //Each Drill creates a whole in the can that beer slowly leaks out of
 
   // Unlocks:
   // Bigger Bits (static)
-  { Title: "Drill", Amount: 0 },
+  { Title: "Drill", Amount: 0, initCost: 1000, SPS: 5 },
+];
+
+const UnlockList: Unlock[] = [
+  {
+    Title: "Fragmented sips",
+    Desc: "Splits all your quantum sips in half, doubling their effectiveness",
+    Trigger: true,
+  },
+  {
+    Title: "Better Friends",
+    Desc: "You scold your friends into drinking more, Makes them sip twice as fast",
+    Trigger: true,
+  },
+  {
+    Title: "Bigger Bits",
+    Desc: "You order larger drill bits to make bigger holes, doubles the size of holes",
+    Trigger: true,
+  },
+  {
+    Title: "Fragmented sips",
+    Desc: "Splits all your quantum sips in half, doubling their effectiveness",
+    Trigger: true,
+  },
+  {
+    Title: "Better Friends",
+    Desc: "You scold your friends into drinking more, Makes them sip twice as fast",
+    Trigger: true,
+  },
+  {
+    Title: "Bigger Bits",
+    Desc: "You order larger drill bits to make bigger holes, doubles the size of holes",
+    Trigger: true,
+  },
 ];
 
 export function Upgrades() {
   return (
-    <div className="flex flex-col w-full h-full">
+    <div className="flex flex-col w-full h-2/3">
       {UpgradeList.map((upgrade, index) => {
         return <Upgrade upgrade={upgrade} key={index} />;
       })}
@@ -57,5 +98,17 @@ export function Upgrade(props: { upgrade: Upgrade }) {
 }
 
 export function Unlocks() {
-  return <></>;
+  return (
+    <div className="flex flex-row flex-wrap w-full h-1/3 content-start">
+      {UnlockList.map((unlock, index) => {
+        if (unlock.Trigger) {
+          return <Unlock unlock={unlock} key={index} />;
+        }
+      })}
+    </div>
+  );
+}
+
+export function Unlock(props: { unlock: Unlock }) {
+  return <div className="w-11 h-11 bg-white border-2 border-black"></div>;
 }
